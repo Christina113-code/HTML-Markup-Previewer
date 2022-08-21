7068f735c322fadefa498832dc49b12ab2f751af
Christina <christinajun113@gmail.com>
2022-08-21 08:24:34 -0400

    HTML feature working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-
+import React, {useState} from 'react';
 function App() {
+  const [input, setInput] = useState('');
+  const handleChange = event =>{
+    setInput(event.target.value);
+    console.log(input);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>HTML Markup Previewer</h2>
         <div className="textarea">
-        <textarea className = "input" placeholder="Type some HTML YAyyyyyy"></textarea>
-        <textarea className = "output" ></textarea>
+        <textarea className = "input box" placeholder="Type some HTML and see it displayed on the right --->" onChange={handleChange}></textarea>
+        <div className = "output box" dangerouslySetInnerHTML={{__html:input}} ></div>
         </div>
        
 
